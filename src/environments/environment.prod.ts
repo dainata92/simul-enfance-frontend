acd956d1f746ac70b93cc3551f85f354390762e7
Dainata Leva <dainataleva@gmail.com>
2026-05-11 20:45:14 +0200

    déploiement Render

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,11 +1,8 @@
 /**
  * Configuration de l'environnement de production
- *
- * Ce fichier sera utilisé lors du build de production.
- * Pour déploiement Docker local : http://localhost:8080/api
- * Pour serveur distant : https://votre-domaine.com/api
+ * URL Render à modifier après déploiement du backend
  */
 export const environment = {
   production: true,
-  apiUrl: 'http://localhost:8080/api'
+  apiUrl: 'https://simul-enfance-backend.onrender.com/api'
 };
